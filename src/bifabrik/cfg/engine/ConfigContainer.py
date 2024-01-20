7004c3f94d2c97e5dc8aa62f8b1c68e2f9169ecb
Radovan Jankovic <radovan.jankovic@givet.it>
2024-01-20 23:07:11 +0100

    direct fluent config setters

--- src/bifabrik/cfg/engine/ConfigContainer.py
+++ src/bifabrik/cfg/engine/ConfigContainer.py
@@ -3,27 +3,45 @@ from bifabrik.cfg.engine.Configuration import CfgProperty
 
 class ConfigContainer:
     def __init__(self):
+        # mapping cfg property names from inenr configurations
+        # to the Configuration objects
         self.__propDict = dict()
-        self.getList()
+        self.setCfgOptions()
     
-    def getList(self):
-        a = dir(self)
-        dnu = list(filter(lambda x: not x.startswith("_"), a))
-        #res = dict()
-        for e in dnu:
-            attr = getattr(self, e)
-            attrType = type(attr)
+    def setCfgOptions(self):
+        rootAttribs = dir(self)
+        noUnderscoreRootAttrNames = list(filter(lambda x: not x.startswith("_"), rootAttribs))
+        
+        # for adding config options from the partial configs
+        self.__doc__ = self.__doc__ + '\n\nConfiguration setters:'
+
+        for rootAttrName in noUnderscoreRootAttrNames:
+            rootAttr = getattr(self, rootAttrName)
+            attrType = type(rootAttr)
             if attrType.__bases__[0].__name__ == 'Configuration':
-                cfgProps = dir(attrType)
-                for cfgp in cfgProps:
-                    cfgatt = getattr(attrType, cfgp)
-                    if isinstance(cfgatt, CfgProperty):
-                        #print("PROP " + cfgp + " in " + e + " (" + str(attr) + ")")
-                        #print(cfgp)
-                        self.__propDict[cfgp] = attr
-        #self.__propDict = res
-        #print(res)
-    
+                cfgPropNames = dir(attrType)
+                for cfgpName in cfgPropNames:
+                    cfgAttribute = getattr(attrType, cfgpName)
+                    if isinstance(cfgAttribute, CfgProperty):
+                        self.__propDict[cfgpName] = rootAttr
+
+                        # create a direct setter property for overriding the value in a fluent API
+                        # so that we don't need to use .option()
+                        # (however, don't override existent properties while doint this - rather ommit some, they will still be available through .ption()_
+
+                        hasA = hasattr(self, cfgpName)
+                        if hasA == False:
+                            def directSetter(val):
+                                self.option(cfgpName, val)
+                                return self
+                            directSetter.__doc__ = f'{cfgAttribute.__doc__} \n(from {rootAttrName} configuration)'
+                            directSetter.__name__ = cfgpName
+                            setattr(self, cfgpName, directSetter)
+
+                        # (don't use this - the __doc__ is in the type)
+                        # instanceAttr = getattr(attr, cfgp)
+                        self.__doc__ = self.__doc__ + f'\n{cfgpName}: {cfgAttribute.__doc__}\n'
+
        
     def option(self, name, value = None):
         if not (name in self.__propDict):
@@ -43,13 +61,6 @@ class ConfigContainer:
     
     def merge(self, other):
         """Meges another configuration into this one; the 'other' config container takes priority"""
-        #print("self")
-        #for key in self.__propDict:
-        #    print(self.__propDict[key]._explicitProps)
-        #print("other")
-        #for key in other.__propDict:
-        #    print(other.__propDict[key]._explicitProps)
-
         for key in other.propDict:
             # that have an explicit value set in the source
             if key in other.propDict[key]._explicitProps:
