37d73672b982be2131733aabca928e739b00f294
Radovan Jankovic <radovan.jankovic@givet.it>
2024-02-03 15:40:31 +0100

    json config

--- src/bifabrik/cfg/engine/ConfigContainer.py
+++ src/bifabrik/cfg/engine/ConfigContainer.py
@@ -33,6 +33,8 @@ class ConfigContainer:
                 for cfgpName in cfgPropNames:
                     cfgAttribute = getattr(attrType, cfgpName)
                     if isinstance(cfgAttribute, CfgProperty):
+                        if cfgpName in __propDict[cfgpName]:
+                            raise Exception(f'bifabrik configuration conflict: {cfgpName} is defined both in {__propDict[cfgpName]} ({type(__propDict[cfgpName])}) and {rootAttr} ({type(rootAttr)})')
                         self.__propDict[cfgpName] = rootAttr
 
                         # create a direct setter property for overriding the value in a fluent API
--- src/bifabrik/src/JsonSource.py
+++ src/bifabrik/src/JsonSource.py
@@ -1,13 +1,15 @@
 from bifabrik.src.DataSource import DataSource
+from bifabrik.cfg.JsonSourceConfiguration import JsonSourceConfiguration
 from pyspark.sql.dataframe import DataFrame
 from bifabrik.utils import fsUtils
 
-class JsonSource(DataSource):
+class JsonSource(DataSource, JsonSourceConfiguration):
     """CSV data source
     """
     
     def __init__(self, parentPipeline):
         super().__init__(parentPipeline)
+        JsonSourceConfiguration.__init__(self)
         self._path = ""
     
     def path(self, path: str):
@@ -18,11 +20,25 @@ class JsonSource(DataSource):
         self._path = path
         return self
     
+    # if key in other.propDict[key]._explicitProps:
+    # df = spark.read.option("multiLine", "true").option('mode', 'PERMISSIVE').json("Files/JSON/ITA_TabZakazka.json")
+
     def execute(self, input) -> DataFrame:
+        mergedConfig = self._pipeline.configuration.mergeToCopy(self)
+
+        # find source files
         source_files = fsUtils.filePatternSearch(self._path)
         if len(source_files) == 0:
             return None
-        df = self._spark.read.option("multiline", "true").json(source_files)
+        
+        # set spark options for all the configuration switches specified in the task's config
+        readerBase = self._spark.read
+        for key in mergedConfig.fileSource._explicitProps:
+            readerBase.option(key, mergedConfig.fileSource['key'])
+        for key in mergedConfig.json._explicitProps:
+            readerBase.option(key, mergedConfig.json['key'])
+
+        df = readerBase.json(source_files)
         
         self._result = df
         self._completed = True
