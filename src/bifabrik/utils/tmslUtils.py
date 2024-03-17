d03a8f512ff339a6b84faf2683dd5f53074ca8dc
Radovan Jankovic <radovan.jankovic@givet.it>
2024-03-17 17:58:36 +0100

    tmslUtils stub

---
+++ src/bifabrik/utils/tmslUtils.py
@@ -0,0 +1,10 @@
+"""
+PowerBI dataset scripting utilities - tabular model backup & restore, removing objects, etc.
+"""
+
+import bifabrik.utils.fsUtils
+import bifabrik.utils.log
+
+
+def backupDataset(sourceWorkspace: str, sourceDataset: str, targetFileName = None):
+    pass
