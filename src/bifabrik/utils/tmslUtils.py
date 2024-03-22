85923d2dc8ad40f99a2791b61d651c14545bdb81
Radovan Jankovic <radovan.jankovic@givet.it>
2024-03-22 23:37:31 +0100

    backup / restore docs

--- src/bifabrik/utils/tmslUtils.py
+++ src/bifabrik/utils/tmslUtils.py
@@ -64,10 +64,24 @@ def backupDataset(sourceDataset: str, sourceWorkspace: str, targetFilePath = Non
 
     tmsl = spf.get_tmsl(dataset = sourceDataset, workspace = sourceWorkspace)
     
-    #print(f'Writing TMSL to {targetFilePath}')
+    print(f'Saving backup to {targetFilePath}')
     notebookutils.mssparkutils.fs.put(targetFilePath, tmsl)
 
 def restoreDataset(sourceFilePath: str, targetDatasetName: str, targetWorkspaceName: str):
+    """Restore a dataset from backup. For creating the backup, use the backupDataset() function.
+    If a dataset with the target name already exists in the workspace, it will be overwritten.
+    If it does not exist, a new dataset will be created.
+
+    Parameters:
+        sourceFilePath (str):       Relative Spark path to the backup created by backupDataset()
+        targetDatasetName (str):    Name of the restored dataset
+        targetWorkspaceName (str):  Name of the workspace where to place the dataset.
+    
+    Examples
+    --------
+    >>> import bifabrik.utils.tmslUtils as tmsl
+    >>> tmsl.restoreDataset(sourceFilePath = 'Files/tmsl_backup/Experimental/DS_R1/DS_R1_2024_03_22_22_05_57_580830.json', targetDatasetName = 'DS_R1_restored', targetWorkspaceName = 'Experimental')
+    """
     datasets_pandas = spf.list_datasets(workspace = targetWorkspaceName)
     spark = pss.SparkSession.builder.getOrCreate()
     datasets = spark.createDataFrame(datasets_pandas)
@@ -76,10 +90,12 @@ def restoreDataset(sourceFilePath: str, targetDatasetName: str, targetWorkspaceN
     
     # look for the destination dataset. If it already exists, take its ID (replace target)
     # otherwise, we're creating a new dataset
-    
+
     ds_filter = datasets.filter(col("Dataset Name") == targetDatasetName)
+    create_new = False
     if ds_filter.count() > 0:
         target_id = ds_filter.collect()[0]['Dataset ID']
+        create_new = True
     
     rdd = spark.read.text(sourceFilePath)
     tmsl_def = ""
@@ -89,15 +105,15 @@ def restoreDataset(sourceFilePath: str, targetDatasetName: str, targetWorkspaceN
     for l in rdd.collect():
         s = l[0]
         if s.strip().startswith('"name":') and name_replaced == False:
-            print('orig ' + s)
+            # print('orig ' + s)
             s = f'"name": "{targetDatasetName}",'
             name_replaced = True
-            print(s)
+            # print(s)
         if s.strip().startswith('"id":') and id_replaced == False:
-            print('orig ' + s)
+            # print('orig ' + s)
             s = f'"id": "{target_id}",'
             id_replaced = True
-            print(s)
+            # print(s)
         tmsl_def = tmsl_def + '\n' + s
 
     wrap_start = f"""{{
@@ -109,4 +125,9 @@ def restoreDataset(sourceFilePath: str, targetDatasetName: str, targetWorkspaceN
     wrap_end = " } }"
     tmsl_cmd = wrap_start + tmsl_def + wrap_end
 
+    if create_new:
+        print(f'Creating dataset {targetDatasetName} in workspace {targetWorkspaceName}')
+    else:
+        print(f'Overwriting dataset {targetDatasetName} in workspace {targetWorkspaceName}')
+    
     spf.execute_tmsl(script = tmsl_cmd, workspace = targetWorkspaceName)
