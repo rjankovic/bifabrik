19717d75951fa9f96fa0971d78e515b0b82668fe
Radovan Jankovic <radovan.jankovic@givet.it>
2024-04-26 14:56:45 +0200

    canAddNewColumns switch support for DW table destination

--- src/bifabrik/dst/WarehouseTableDestination.py
+++ src/bifabrik/dst/WarehouseTableDestination.py
@@ -249,6 +249,10 @@ WHERE s.name = '{self.__targetSchemaName}' AND t.name = '{self.__targetTableName
             raise Exception(f'Cannot resolve schema chnages in table [{self.__targetSchemaName}].[{self.__targetTableName}], column {broken_column_name}({broken_column_type}) - {type_error}')
             
         if schema_change:
+            canAddColumns = self.__tableConfig.canAddNewColumns
+            if not canAddColumns:
+                raise Exception(f'Cannot resolve schema chnages in destination table [{self.__targetSchemaName}].[{self.__targetTableName}] because the `canAddNewColumns` setting is False')
+            
             msg = f'Rebuilding table [{self.__targetSchemaName}].[{self.__targetTableName}], to add new columns...'
             self.__logger.info(msg)
             print(msg)
